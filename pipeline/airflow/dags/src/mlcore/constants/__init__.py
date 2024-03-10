2b4709b6258309ebebddb7303c5feadd13ff168f
Debanjan Saha <debanjansh@gmail.com>
2024-03-10 17:09:15 -0400

    restructured entire folder structure

---
+++ pipeline/airflow/dags/src/mlcore/constants/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("src/mlcore/config/config.yaml")
+PARAMS_FILE_PATH = Path("src/mlcore/constants/params.yaml")
+SCHEMA_FILE_PATH = Path("src/mlcore/constants/schema.yaml")
