8e2f3d0d6ef3f2bfb866f6dec3529c5f7c1ed7fc
Akhil Krishna <akhilkn8@gmail.com>
2024-03-11 19:39:42 -0400

    Added path to gcs_key.json in mlcore/constants

--- pipeline/airflow/dags/src/mlcore/constants/__init__.py
+++ pipeline/airflow/dags/src/mlcore/constants/__init__.py
@@ -3,4 +3,4 @@ from pathlib import Path
 CONFIG_FILE_PATH = Path("src/mlcore/config/config.yaml")
 PARAMS_FILE_PATH = Path("src/mlcore/constants/params.yaml")
 SCHEMA_FILE_PATH = Path("src/mlcore/constants/schema.yaml")
-GCSKEY_FILE_PATH = Path("speech-emotion-recognition/pipeline/airflow/secrets/gcs_key.json")
+GCSKEY_FILE_PATH = Path("/opt/airflow/config/gcs_key.json")
