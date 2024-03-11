5091139e6b12d0211d8815190aeec156c544866b
Akhil Krishna <akhilkn8@gmail.com>
2024-03-11 19:39:12 -0400

    Updated Dag with upload to GCP func tasks

--- pipeline/airflow/dags/data_pipeline.py
+++ pipeline/airflow/dags/data_pipeline.py
@@ -7,32 +7,15 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow import configuration as conf
-from src.gcp import authenticate, upload_directory
+from gcp import upload_directory
 
 from stage_01_data_ingestion import DataIngestionTrainingPipeline
 from stage_02_data_validation import DataValidationTrainingPipeline
-# from stage_03_data_transformation import DataTransformationTrainingPipeline
-# from src.data_collection.data_collection import (
-#     prepare_folders,
-#     copy_ravdess_dataset,
-#     copy_meld_dataset,
-# )
+from stage_03_data_transformation import DataTransformationTrainingPipeline
+
 
 # Enable pickle support for XCom, allowing data to be passed between tasks
 conf.set("core", "enable_xcom_pickling", "True")
-
-
-# Define Python Functions
-def python_function():
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
-
-
-def copy_data():
-    print("Blank function")
-    # prepare_folders()
-    # copy_ravdess_dataset()
-
 
 # Start Dag Definition
 default_args = {
@@ -59,11 +42,18 @@ dag = DAG(
 
 start_pipeline = BashOperator(task_id="start_pipeline", bash_command="pwd && ls -lart")
 
-download_data = PythonOperator(
-    task_id="download_data",
+data_ingestion = PythonOperator(
+    task_id="data_ingestion",
     python_callable=DataIngestionTrainingPipeline().main,
     provide_context=True,
     dag=dag,
+)
+
+upload_data_ingestion = PythonOperator(
+    task_id="upload_data_ingestion", 
+    python_callable=upload_directory,
+    op_args = ['mlops-grp3-data-bucket', '/opt/airflow/dags/src/mlcore/artifacts', '', "/opt/airflow/config/gcs_key.json"] ,
+    dag=dag
 )
 
 data_validation = PythonOperator(
@@ -73,46 +63,41 @@ data_validation = PythonOperator(
     dag=dag,
 )
 
-authenticate = PythonOperator(
-    task_id="authenticate", 
-    python_callable=authenticate, 
-    op_args= ["speech-emotion-recognition/pipeline/airflow/secrets/gcs_key.json"],
-    dag=dag
-)
-
-upload_directory = PythonOperator(
-    task_id="upload_directory", 
+upload_data_validation = PythonOperator(
+    task_id="upload_data_validation", 
     python_callable=upload_directory,
-    op_args = ['mlops-grp3-data-bucket', 'mlops-group3/data'] ,
+    op_args = ['mlops-grp3-data-bucket', '/opt/airflow/dags/src/mlcore/artifacts/data_validation', 'data_validation/', "/opt/airflow/config/gcs_key.json"] ,
     dag=dag
 )
 
 
-# data_transformation = PythonOperator(
-#     task_id="data_transformation",
-#     python_callable=DataTransformationTrainingPipeline().main,
-#     provide_context=True,
-#     dag=dag,
-# )
+data_transformation = PythonOperator(
+    task_id="data_transformation",
+    python_callable=DataTransformationTrainingPipeline().main,
+    provide_context=True,
+    dag=dag,
+)
 
-# upload_data = PythonOperator(
-#     task_id="upload_data",
-#     python_callable=python_function,
-#     provide_context=True,
-#     dag=dag,
-# )
+upload_data_transformation = PythonOperator(
+    task_id="upload_data_transformation", 
+    python_callable=upload_directory,
+    op_args = ['mlops-grp3-data-bucket', '/opt/airflow/dags/src/mlcore/artifacts/data_transformation', 'data_transformation/', "/opt/airflow/config/gcs_key.json"] ,
+    dag=dag
+)
 
-# end_pipeline = EmptyOperator(
-#     task_id="end_pipeline",
-#     dag=dag,
-# )
+end_pipeline = EmptyOperator(
+    task_id="end_pipeline",
+    dag=dag,
+)
 
 # Set dependencies between tasks
 (
     start_pipeline
-    >> download_data
+    >> data_ingestion
+    # >> upload_data_ingestion
     >> data_validation
-    # >> data_transformation
-    # >> upload_data
-    # >> end_pipeline
+    # >> upload_data_validation
+    >> data_transformation
+    >> upload_data_transformation
+    >> end_pipeline
 )
