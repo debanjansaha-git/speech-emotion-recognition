016509dff01ebd43cd6927402f1820a051627726
Venkatesh Gopinath <venkateshgopi24@gmail.com>
2024-03-13 00:50:34 -0400

    Added Security alert for dag sucess or fail

--- pipeline/airflow/dags/data_pipeline.py
+++ pipeline/airflow/dags/data_pipeline.py
@@ -22,12 +22,31 @@ default_args = {
     "owner": "airflow",
     "depends_on_past": False,
     "start_date": datetime.now(),
-    # 'email': ['airflow@example.com'],
-    "email_on_failure": False,
-    "email_on_retry": False,
     "retries": 0,
     "retry_delay": timedelta(minutes=5),
 }
+
+# Define function to notify failure or sucess via an email
+def notify_success(context):
+    success_email = EmailOperator(
+        task_id='success_email',
+        to='venkateshgopi24@gmail.com',
+        subject='Success Notification from Airflow',
+        html_content='<p>The task ran sucessfully.</p>',
+        dag=context['dag']
+    )
+    success_email.execute(context=context)
+
+def notify_failure(context):
+    failure_email = EmailOperator(
+        task_id='failure_email',
+        to='venkateshgopi24@gmail.com',
+        subject='Failure Notification from Airflow',
+        html_content='<p>The task failed.</p>',
+        dag=context['dag']
+    )
+    failure_email.execute(context=context)
+
 
 # A DAG represents a workflow, a collection of tasks
 dag = DAG(
@@ -39,6 +58,18 @@ dag = DAG(
 )
 
 # Define Operators
+
+# Define the email task
+send_email = EmailOperator(
+    task_id='send_email',
+    to='venkateshgopi24@gmail.com',    # Email address of the recipient
+    subject='Notification from Airflow',
+    html_content='<p>This is a notification email sent from Airflow.</p>',
+    dag=dag,
+    on_failure_callback=notify_failure,
+    on_success_callback=notify_success
+)
+
 
 start_pipeline = BashOperator(task_id="start_pipeline", bash_command="pwd && ls -lart")
 
@@ -96,5 +127,5 @@ with dag:
     data_ingestion >> [data_validation, upload_data_ingestion]
     data_validation >> [data_transformation, upload_data_validation]
     data_transformation >> upload_data_transformation
-    upload_data_transformation >> end_pipeline
+    upload_data_transformation >> end_pipeline >> send_email
     
