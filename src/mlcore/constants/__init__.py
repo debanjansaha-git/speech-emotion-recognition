af12e5cdc5d53052873108bc8f09aeed0a9887ab
Akhil Krishna <akhilkn8@gmail.com>
2024-04-05 14:15:45 -0400

    data transformation updates and fixes

--- src/mlcore/constants/__init__.py
+++ src/mlcore/constants/__init__.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 """Contains path to configuration files"""
 
-CONFIG_FILE_PATH = Path("src/mlcore/config/config.yaml")
-PARAMS_FILE_PATH = Path("src/mlcore/constants/params.yaml")
-SCHEMA_FILE_PATH = Path("src/mlcore/constants/schema.yaml")
+CONFIG_FILE_PATH = Path("/opt/airflow/dags/mlcore/config/config.yaml")
+PARAMS_FILE_PATH = Path("/opt/airflow/dags/mlcore/constants/params.yaml")
+SCHEMA_FILE_PATH = Path("/opt/airflow/dags/mlcore/constants/schema.yaml")
 GCSKEY_FILE_PATH = Path("/opt/airflow/config/gcs_key.json")
